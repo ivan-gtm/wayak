09c79be094b4c71fd602c45c6d5d0cd57f3f1f8d
Daniel Gutierrez <dagtok@gmail.com>
2019-07-14 17:01:39 -0500

    Se crea JOB para subir en bulk a PROD desde laravel via comando.
    Se crea JOB para subir en bulk a PROD desde laravel via comando.

--- app/Console/Commands/UploadTemplateAssetsToS3.php
+++ app/Console/Commands/UploadTemplateAssetsToS3.php
@@ -3,6 +3,12 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Storage;
+// use Illuminate\Support\Facades\Storage;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Str;
+
 
 class UploadTemplateAssetsToS3 extends Command
 {
@@ -11,14 +17,14 @@ class UploadTemplateAssetsToS3 extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'command:upload_assets_s3';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Upload assets to S3';
 
     /**
      * Create a new command instance.
@@ -37,6 +43,21 @@ class UploadTemplateAssetsToS3 extends Command
      */
     public function handle()
     {
-        //
+        // $path = Storage::disk('s3')->put('images/originals', $request->file);
+        $file = 'https://dsnegsjxz63ti.cloudfront.net/images/homeimages/landing/325cffc8df41a64.jpeg';
+        $imageFileName = '325cffc8df41a64.jpeg';
+        $filePath = '/billionbillion/' . $imageFileName;
+        Storage::disk('s3')->put($filePath, file_get_contents($file),'public');
+
+        // $s3 = \Storage::disk('s3');
+        // $filePath = '/support-tickets/' . $imageFileName;
+        // $s3->put($filePath, file_get_contents($image), 'public');
+
+        // $request->merge([
+        //     'size' => $request->file->getClientSize(),
+        //     'path' => $path
+        // ]);
+        // $this->image->create($request->only('path', 'title', 'size'));
+        // return back()->with('success', 'Image Successfully Saved');    
     }
 }
