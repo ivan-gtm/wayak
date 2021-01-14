3a82a683f5fcd28ce0ed34ef83d54a148bc46e29
Daniel Gutierrez <ivan.daniel.gtz@gmail.com>
2021-01-14 11:19:36 -0600

    Fix templates on sale
    Fix templates on sale

--- resources/views/admin/templates.blade.php
+++ resources/views/admin/templates.blade.php
@@ -1,62 +1,56 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+@section('title', 'Panel de Administración')
 
-    <title>Codigos</title>
-    <style>
-      .box {
-        position: relative;
-        margin: 20px auto;
-        /* width: 400px;
-        height: 350px; */
-        background: #fff;
-        border-radius: 2px;
-      }
+@section('css')
+  <style>
+        .box {
+          position: relative;
+          margin: 20px auto;
+          /* width: 400px;
+          height: 350px; */
+          background: #fff;
+          border-radius: 2px;
+        }
 
-      .box::before,
-      .box::after {
-        content: '';
-        position: absolute;
-        bottom: 10px;
-        width: 40%;
-        height: 10px;
-        box-shadow: 0 5px 14px rgba(0,0,0,.7);
-        z-index: -1;
-        transition: all .3s ease-in-out;
-      }
+        .box::before,
+        .box::after {
+          content: '';
+          position: absolute;
+          bottom: 10px;
+          width: 40%;
+          height: 10px;
+          box-shadow: 0 5px 14px rgba(0,0,0,.7);
+          z-index: -1;
+          transition: all .3s ease-in-out;
+        }
 
-      .box::before {
-        left: 15px;
-        transform: skew(-5deg) rotate(-5deg);
-      }
+        .box::before {
+          left: 15px;
+          transform: skew(-5deg) rotate(-5deg);
+        }
 
-      .box::after {
-        right: 15px;
-        transform: skew(5deg) rotate(5deg);
-      }
+        .box::after {
+          right: 15px;
+          transform: skew(5deg) rotate(5deg);
+        }
 
-      .box:hover::before,
-      .box:hover::after {
-        box-shadow: 0 2px 14px rgba(0,0,0,.4);
-      }
+        .box:hover::before,
+        .box:hover::after {
+          box-shadow: 0 2px 14px rgba(0,0,0,.4);
+        }
 
-      .box:hover::before {
-        left: 5px;
-      }
+        .box:hover::before {
+          left: 5px;
+        }
 
-      .box:hover::after {
-        right: 5px;
-      }
-    </style>
-  </head>
-  <body>
+        .box:hover::after {
+          right: 5px;
+        }
+  </style>
+@endsection
 
+@section('content')
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
@@ -78,29 +72,18 @@
                           {{ $product_info['title'] }}<br>
                           {{ $product_info['dimentions'] }}</p>
                       </div>
-                      <div class="col-4">
-                        <a href="{{ route('code.create', [
+                      <div class="col-12">
+                        <a href="{{ route('admin.edit.template', [
+                            'language_code' => $language_code,
+                            'template_key' => $product_info['key']
+                        ]) }}">EDITAR PLANTILLA</a>
+                      </div>
+                      <div class="col-12">
+                        <br>
+                        <a href="{{ route('plantilla.demo', [
+                          'modelo_mercado_pago' => $product_info['mercadopago'],
                           'country' => $country,
-                          'code' => $product_info['key']
-                        ] ) }}">
-                          GENERAR CODIGO DEMO
-                        </a>
-                      </div>
-                      <div class="col-4">
-                        @if( $product_info['translation_ready'] )
-                          <a href="{{ route('admin.edit.template', [
-                              'language_code' => $language_code,
-                              'template_key' => $product_info['key']
-                          ]) }}">EDITAR PLANTILLA</a>
-                        @endif
-                      </div>
-                      <div class="col-4">
-                        @if( $product_info['mercadopago'] > 0 )
-                          <a href="{{ route('plantilla.demo', [
-                            'modelo_mercado_pago' => $product_info['mercadopago'],
-                            'country' => $country,
-                          ] ) }}">DEMO MP</a>
-                        @endif
+                        ] ) }}">VER DEMO</a>
                       </div>
                     </div>
                 </div>
@@ -147,15 +130,6 @@
           </div>
         </div>
     </div>
+@endsection
+    
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper.js -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper.js and Bootstrap JS
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js" integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
